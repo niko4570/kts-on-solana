2c7c0a71fe83beeb94f22732ecd985306fc5f05a
niko <niko24561@gmail.com>
2026-01-16 13:54:21 +0800

    complete new features:register_device

--- programs/kts/src/instructions/register_device.rs
+++ programs/kts/src/instructions/register_device.rs
@@ -8,11 +8,29 @@ pub fn register_device(
     device_hash:[u8;32],
     device_name:String,
 )->Result<()>{
-    
+    require!(device_name.len()<=64,ktsError::InvalidProcessNameLength);
+    let device_account=&mut ctx.accounts.device_account;
+    device_account.owner=*ctx.accounts.user.key;
+    device_account.device_hash=device_hash;
+    device_account.registered_at=Clock::get()?.unix_timestamp;
+    device_account.nft_minted=false;
     Ok(())
 }
 
 #[derive(Accounts)]
-pub struct RegisterDevice{
-    
+#[instruction(device_hash:[u8;32])]
+pub struct RegisterDevice<'info>{  
+    #[account(mut)]
+    pub user:Signer<'info>,
+
+    #[account(
+        init,
+        payer=user,
+        space=DEVICE_ACCOUNT_SIZE,
+        seeds=[b"device",device_hash.as_ref()],
+        bump,
+    )]
+    pub device_account:Account<'info,DeviceAccount>,
+    pub system_program:Program<'info,System>,
+
 }

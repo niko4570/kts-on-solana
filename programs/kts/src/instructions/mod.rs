b6f2cb8acdbfb8a11b3f7a46762e2f400bb4ccdd
niko <niko24561@gmail.com>
2026-01-16 14:22:48 +0800

    fix import issue

--- programs/kts/src/instructions/mod.rs
+++ programs/kts/src/instructions/mod.rs
@@ -1,3 +1,7 @@
 pub mod register_device;
 pub mod upload_daily_usage;
 pub mod mint_nft;
+
+pub use register_device::*;
+pub use upload_daily_usage::*;
+pub use mint_nft::*;
--- programs/kts/src/lib.rs
+++ programs/kts/src/lib.rs
@@ -5,14 +5,20 @@ pub mod instructions;
 pub mod state;
 pub mod errors;
 
-use instructions::*;
-use state::*;
 declare_id!("GUMBiEgcTL58Sz7wKoWWQyiUoFisbZ6ELvt2ZyoyEJ5G");
 
+use instructions::*;
+use state::*;
 
 #[program]
 
 pub mod kts {
     use super::*;
-
+    pub fn register_device(
+        ctx:Context<RegisterDevice>,
+        device_hash:[u8;32],
+        device_name:String,
+    ) ->Result<()>{
+        instructions::register_device(ctx,device_hash,device_name)
+    }
 }

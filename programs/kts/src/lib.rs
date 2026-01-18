f8d24e640593a71045a1446caef4ce7a61664a75
niko <niko24561@gmail.com>
2026-01-18 13:52:11 +0800

    write lib.rs

--- programs/kts/src/lib.rs
+++ programs/kts/src/lib.rs
@@ -8,7 +8,6 @@ pub mod errors;
 declare_id!("GUMBiEgcTL58Sz7wKoWWQyiUoFisbZ6ELvt2ZyoyEJ5G");
 
 use instructions::*;
-use state::*;
 
 #[program]
 
@@ -20,5 +19,28 @@ pub mod kts {
         device_name:String,
     ) ->Result<()>{
         instructions::register_device(ctx,device_hash,device_name)
+    }
+
+    pub fn upload_daily_usage(
+        ctx:Context<UploadDailyUsage>,
+        device_hash:[u8;32],
+        timestamp:i64,
+        avg_cpu_usage:f32,
+        avg_memory_usage:f32,
+        top_processes:Vec<String>,
+        data_hash:[u8;32],
+    )->Result<()>{
+        instructions::upload_daily_usage(
+            ctx, 
+            device_hash,
+            timestamp, 
+            avg_cpu_usage, 
+            avg_memory_usage, 
+            top_processes, 
+            data_hash)
+    }
+
+    pub fn mark_nft_minted(ctx:Context<MarkNftMinted>)->Result<()>{
+        instructions::mark_nft_minted(ctx)
     }
 }
